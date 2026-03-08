baa3fc6f7dda0a3cd1f37b5baf67d40f09a24e09
Bárbara Sagredo <barpaz.sc@gmail.com>
2026-03-08 19:08:13 -0300

    se modifica el icono de la pagina

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
 
   return (
     <div className="App">
-      <h2 className="py-1 my-4 text-start ms-5">  ⌞ ⌝ Like Me</h2>
+      <h2 className="py-1 my-4 text-start ms-5">  ⌞⌝ Like Me</h2>
       <div className="row m-auto px-5">
         <div className="col-12 col-sm-4 mb-3">
           <Form
